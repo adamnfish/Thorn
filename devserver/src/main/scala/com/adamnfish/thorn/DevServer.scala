ba135d128d762a03b4fd345a818173ac9c62b425
adamnfish <adamnfish@gmail.com>
2020-06-06 18:03:16 +0100

    Improve message display in devserver
    Each ID is assigned a unique bg/fg colour combination to help with
    visually identifying messages in the printed log.
    
    The selection of colours is a subset of those available, chosen for
    legibility and differentiability.

--- devserver/src/main/scala/com/adamnfish/thorn/DevServer.scala
+++ devserver/src/main/scala/com/adamnfish/thorn/DevServer.scala
@@ -9,6 +9,8 @@ import org.scanamo.LocalDynamoDB
 import scala.concurrent.Await
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
+import scala.io.AnsiColor
+import scala.util.Random
 
 
 object DevServer {
@@ -25,16 +27,16 @@ object DevServer {
     app.ws("/api", { ws =>
       ws.onConnect { wctx =>
         val id = messaging.connect(wctx)
-        println(s"Connected: $id")
+        println(s"Connected: ${displayId(id, fullId = true)}")
       }
       ws.onClose { wctx =>
         messaging.disconnect(wctx)
-        println(s"Disconnected: ${wctx.getSessionId}")
+        println(s"Disconnected: ${displayId(wctx.getSessionId)}")
       }
       ws.onMessage { wctx =>
         // TODO: consider whether errors are sent as messages or responses?
         // (start with messages for simplicity, change to response in the future to save $$)
-        println(s"Message: ${wctx.getSessionId} <- ${wctx.message}")
+        println(s"Message: ${displayId(wctx.getSessionId)} <- ${wctx.message}")
         val context = Context(PlayerAddress(wctx.getSessionId), db, messaging)
         val result = Thorn.main(wctx.message, context).tapFailure { failure =>
           println(s"[ERROR] ${failure.logString}")
@@ -48,4 +50,51 @@ object DevServer {
       app.stop()
     }))
   }
+
+  private def displayId(id: String, fullId: Boolean = false): String = {
+    val seed = id.filterNot(_ == '-').sum
+    val rng = new Random(seed)
+    val colourCode = rng.shuffle(colours).head
+    val idSummary =
+        if (fullId) id
+        else id.take(8)
+    s"$colourCode$idSummary${AnsiColor.RESET}"
+  }
+
+  private val colours = List(
+    s"${AnsiColor.RED_B}${AnsiColor.BOLD}${AnsiColor.BLACK}",
+    s"${AnsiColor.RED_B}${AnsiColor.BOLD}${AnsiColor.GREEN}",
+    s"${AnsiColor.RED_B}${AnsiColor.BOLD}${AnsiColor.YELLOW}",
+    s"${AnsiColor.RED_B}${AnsiColor.BOLD}${AnsiColor.MAGENTA}",
+    s"${AnsiColor.RED_B}${AnsiColor.BOLD}${AnsiColor.CYAN}",
+    s"${AnsiColor.RED_B}${AnsiColor.BOLD}${AnsiColor.WHITE}",
+    s"${AnsiColor.GREEN_B}${AnsiColor.BOLD}${AnsiColor.BLACK}",
+    s"${AnsiColor.GREEN_B}${AnsiColor.BOLD}${AnsiColor.RED}",
+    s"${AnsiColor.GREEN_B}${AnsiColor.BOLD}${AnsiColor.YELLOW}",
+    s"${AnsiColor.GREEN_B}${AnsiColor.BOLD}${AnsiColor.BLUE}",
+    s"${AnsiColor.GREEN_B}${AnsiColor.BOLD}${AnsiColor.MAGENTA}",
+    s"${AnsiColor.GREEN_B}${AnsiColor.BOLD}${AnsiColor.WHITE}",
+    s"${AnsiColor.YELLOW_B}${AnsiColor.BOLD}${AnsiColor.BLACK}",
+    s"${AnsiColor.YELLOW_B}${AnsiColor.BOLD}${AnsiColor.RED}",
+    s"${AnsiColor.YELLOW_B}${AnsiColor.BOLD}${AnsiColor.BLUE}",
+    s"${AnsiColor.YELLOW_B}${AnsiColor.BOLD}${AnsiColor.MAGENTA}",
+    s"${AnsiColor.YELLOW_B}${AnsiColor.BOLD}${AnsiColor.CYAN}",
+    s"${AnsiColor.YELLOW_B}${AnsiColor.BOLD}${AnsiColor.WHITE}",
+    s"${AnsiColor.BLUE_B}${AnsiColor.BOLD}${AnsiColor.BLACK}",
+    s"${AnsiColor.BLUE_B}${AnsiColor.BOLD}${AnsiColor.RED}",
+    s"${AnsiColor.BLUE_B}${AnsiColor.BOLD}${AnsiColor.YELLOW}",
+    s"${AnsiColor.BLUE_B}${AnsiColor.BOLD}${AnsiColor.CYAN}",
+    s"${AnsiColor.BLUE_B}${AnsiColor.BOLD}${AnsiColor.WHITE}",
+    s"${AnsiColor.MAGENTA_B}${AnsiColor.BOLD}${AnsiColor.BLACK}",
+    s"${AnsiColor.MAGENTA_B}${AnsiColor.BOLD}${AnsiColor.RED}",
+    s"${AnsiColor.MAGENTA_B}${AnsiColor.BOLD}${AnsiColor.GREEN}",
+    s"${AnsiColor.MAGENTA_B}${AnsiColor.BOLD}${AnsiColor.YELLOW}",
+    s"${AnsiColor.MAGENTA_B}${AnsiColor.BOLD}${AnsiColor.CYAN}",
+    s"${AnsiColor.MAGENTA_B}${AnsiColor.BOLD}${AnsiColor.WHITE}",
+    s"${AnsiColor.CYAN_B}${AnsiColor.BOLD}${AnsiColor.BLACK}",
+    s"${AnsiColor.CYAN_B}${AnsiColor.BOLD}${AnsiColor.RED}",
+    s"${AnsiColor.CYAN_B}${AnsiColor.BOLD}${AnsiColor.YELLOW}",
+    s"${AnsiColor.CYAN_B}${AnsiColor.BOLD}${AnsiColor.MAGENTA}",
+    s"${AnsiColor.CYAN_B}${AnsiColor.BOLD}${AnsiColor.WHITE}",
+  )
 }
