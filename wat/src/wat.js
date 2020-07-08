35bba50e19f8bc71dd40974f3f26e86f4457aaec
adamnfish <adamnfish@gmail.com>
2020-07-09 00:21:43 +0100

    Target buttons properly in WATs

--- wat/src/wat.js
+++ wat/src/wat.js
@@ -21,7 +21,7 @@ const {webkit} = require('playwright');
 
   await creator.screenshot({path: screenshotPath('creator', '01-welcome.png')});
 
-  await creator.click('text=Create game');
+  await creator.click(buttonWithText('Create game'));
   await creator.waitForSelector(ui('create-game'));
 
   await creator.screenshot({path: screenshotPath('creator', '02-1-create-game.png')});
@@ -29,7 +29,7 @@ const {webkit} = require('playwright');
   await creator.fill(labelledInput('Screen name'), "Creator");
   await creator.screenshot({path: screenshotPath('creator', '02-2-create-game-with-input.png')});
 
-  await creator.click("text=Create game");
+  await creator.click(buttonWithText('Create game'));
   await creator.waitForSelector(textFragment('loading'));
 
   await creator.screenshot({path: screenshotPath('creator', '02-3-create-game-loading.png')});
@@ -46,7 +46,7 @@ const {webkit} = require('playwright');
 
   await player1.screenshot({path: screenshotPath('player1', '01-welcome.png')});
 
-  await player1.click('text=Join game');
+  await player1.click(buttonWithText('Join game'));
   await player1.waitForSelector(ui('join-game'));
 
   await player1.screenshot({path: screenshotPath('player1', '02-1-join-game.png')});
@@ -54,7 +54,7 @@ const {webkit} = require('playwright');
   await player1.fill(labelledInput('Screen name'), "Player 1");
   await player1.screenshot({path: screenshotPath('player1', '02-2-join-game-with-input.png')});
 
-  await player1.click("text=Join game");
+  await player1.click(buttonWithText('Join game'));
   await player1.waitForSelector(ui('lobby'));
 
   await player1.screenshot({path: screenshotPath('player1', '03-1-lobby.png')});
@@ -66,7 +66,7 @@ const {webkit} = require('playwright');
 
   await player2.screenshot({path: screenshotPath('player2', '01-welcome.png')});
 
-  await player2.click('text=Join game');
+  await player2.click(buttonWithText('Join game'));
   await player2.waitForSelector(ui('join-game'));
 
   await player2.screenshot({path: screenshotPath('player2', '02-1-join-game.png')});
@@ -74,14 +74,14 @@ const {webkit} = require('playwright');
   await player2.fill(labelledInput('Screen name'), "Player 2");
   await player2.screenshot({path: screenshotPath('player2', '02-2-join-game-with-input.png')});
 
-  await player2.click("text=Join game");
+  await player2.click(buttonWithText('Join game'));
   await player2.waitForSelector(ui('lobby'));
 
   await player2.screenshot({path: screenshotPath('player2', '03-1-lobby.png')});
   await creator.screenshot({path: screenshotPath('creator', '03-3-lobby.png')});
 
   // creator start game
-  await creator.click("text=Start game");
+  await creator.click(buttonWithText('Start game'));
   await creator.waitForSelector(ui('place-disc'));
 
   await creator.screenshot({path: screenshotPath('creator', '04-1-initial-disc-screen.png')});
@@ -93,16 +93,16 @@ const {webkit} = require('playwright');
   await player2.screenshot({path: screenshotPath('player2', '04-1-initial-disc-screen.png')});
 
   // now in-game, initial discs round
-  await creator.click("text=Rose");
-  await creator.click("text=Submit");
+  await creator.click(buttonWithText('Rose'));
+  await creator.click(buttonWithText('Submit'));
   await creator.waitForSelector(ui('display-game'));
 
   await creator.screenshot({path: screenshotPath('creator', '04-2-waiting-for-others.png')});
 
-  await player1.click("text=Rose");
-  await player1.click("text=Submit");
-  await player2.click("text=Rose");
-  await player2.click("text=Submit");
+  await player1.click(buttonWithText('Rose'));
+  await player1.click(buttonWithText('Submit'));
+  await player2.click(buttonWithText('Rose'));
+  await player2.click(buttonWithText('Submit'));
 
   // place discs round
   await creator.waitForSelector(ui('disc-or-bid'));
@@ -114,8 +114,8 @@ const {webkit} = require('playwright');
   await player2.waitForSelector(ui('display-game'));
   await player2.screenshot({path: screenshotPath('player2', '05-1-display-game.png')});
 
-  await creator.click("text=Rose");
-  await creator.click("text=Submit");
+  await creator.click(buttonWithText('Rose'));
+  await creator.click(buttonWithText('Submit'));
   await creator.waitForSelector(ui('display-game'));
   await creator.screenshot({path: screenshotPath('creator', '05-2-player-1-placing.png')});
 
@@ -123,8 +123,8 @@ const {webkit} = require('playwright');
   await player1.screenshot({path: screenshotPath('player1', '05-2-player-1-placing.png')});
 
   // player 1 opens the bidding with the maximum bid
-  await player1.click("text=4");
-  await player1.click("text=Submit");
+  await player1.click(buttonWithText('4'));
+  await player1.click(buttonWithText('Submit'));
   await player1.waitForSelector(ui('flip'));
 
   await creator.screenshot({path: screenshotPath('creator', '06-1-player-1-flipping.png')});
@@ -132,19 +132,19 @@ const {webkit} = require('playwright');
   await player2.screenshot({path: screenshotPath('player2', '06-1-player-1-flipping.png')});
 
   // think about how to handle loading / messages lifecycle in WATs
-//  await player1.click("text=Flip");
+//  await player1.click(buttonWithText('Flip'));
 //  await player1.screenshot({path: screenshotPath('player1', '06-2-player-1-flipping.png')});
-//  await player1.click("text=Submit");
+//  await player1.click(buttonWithText('Submit'));
 //  await player1.screenshot({path: screenshotPath('player1', '06-3-player-1-flipping.png')});
-//  await player1.click("text=Flip");
+//  await player1.click(buttonWithText('Flip'));
 //  await player1.screenshot({path: screenshotPath('player1', '06-4-player-1-flipping.png')});
-//  await player1.click("text=Submit");
+//  await player1.click(buttonWithText('Submit'));
 //  await player1.screenshot({path: screenshotPath('player1', '06-5-player-1-flipping.png')});
-//  await player1.click("text=Flip");
+//  await player1.click(buttonWithText('Flip'));
 //  await player1.screenshot({path: screenshotPath('player1', '06-6-player-1-flipping.png')});
-//  await player1.click("text=Submit");
+//  await player1.click(buttonWithText('Submit'));
 //  await player1.screenshot({path: screenshotPath('player1', '06-7-player-1-flipping.png')});
-//  await player1.click("text=Flip");
+//  await player1.click(buttonWithText('Flip'));
 //  await player1.screenshot({path: screenshotPath('player1', '06-8-player-1-flipping.png')});
 //
 //  // round finished
@@ -182,6 +182,10 @@ const {webkit} = require('playwright');
 
   await browser.close();
 })();
+
+function buttonWithText(text) {
+  return "css=[role=button] >> text=" + text + "";
+}
 
 function labelledInput(label) {
   return "xpath=//*[contains(text(), '" + label + "')]/../..//input";
